85323516c3a99acfead52c932428f7ceade18d48
Diego <dcarrillozamora.d@gmail.com>
2025-04-15 12:17:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
+
 // Routes
 app.use('/auth', authRoutes);
 app.use('/api', customerRoutes);
